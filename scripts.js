ff897a9089d521c27f31fcc6da50dbef81fb0add
Pratyansh Maddhesia <pratyansh35@gmail.com>
2023-08-22 20:47:29 +0530

    second commit

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 function addRecommendation(){
     // Get the message of the new recommendation
     let recommendation = document.getElementById("new_recommendation");
-    // If the user has left a recommendation, display a pop-up
+   
     if (recommendation.value != null && recommendation.value.trim() != "") {
       console.log("New recommendation added");
-      //Call showPopup here
-     showPopup(true);
-      // Create a new 'recommendation' element and set it's value to the user's message
+      
+      showPopup(true);
       var element = document.createElement("div");
       element.setAttribute("class","recommendation");
       element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
