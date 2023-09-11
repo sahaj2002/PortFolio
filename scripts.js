400369eadd5aa13f642ca7a260b6fb7274170a4a
Pratyansh Maddhesia <pratyansh35@gmail.com>
2023-09-11 15:34:48 +0530

    another commit

--- scripts.js
+++ scripts.js
@@ -1,12 +1,71 @@
+var myInterval = setInterval(slideshow, 3000);
+
+function slideshow() {
+    var i = document.getElementById("Imagey");
+
+    if (i.src.match("images/myPic3.png")) {     
+      i.src = "images/myPic2.png";
+    } else if (i.src.match("images/myPic2.png")) { 
+        i.src = "images/myPic4.jpg";
+    } else if (i.src.match("images/myPic4.jpg")) {
+      i.src = "images/myPic3.png";
+  } else {
+        i.src = "images/myPic3.png";
+    }
+}
+
+
+// function cardsb(y) {
+//   y.style.backgroundColor = "orange";
+//   y.style.transition = "all 2s ease-in-out";
+//   // y.style.transform = "rotate(360deg)";
+//   y.style.border = "5px solid black";
+//   y.style.borderRadius = "50%";
+//   y.style.boxShadow = "10px 10px 5px grey";
+//   y.style.height = "56px";
+//   y.style.width = "56px";
+// }
+// function cardsm(y) {
+  
+//   border: 1px solid gray;
+//     display: block;
+//     border-radius: 6px;
+//     text-align: center;
+//     margin: 50px;
+//     /* padding: 10px; */
+//     width: 2in;
+//     font-size: 20px;
+//     box-shadow: 0 3px 10px gray;
+// }
+
+function big(x) {
+
+  x.style.backgroundColor = "orange";
+  x.style.transition = "all 1s ease-in-out";
+  x.style.transform = "rotate(360deg)";
+  x.style.border = "5px solid black";
+  x.style.borderRadius = "50%";
+  x.style.boxShadow = "10px 10px 5px grey";
+  x.style.height = "500px";
+  x.style.width = "500px";
+  }
+  function small(x) {
+  x.style.height = "400px";
+  x.style.width = "400px";
+  x.style.padding = "10px";
+  x.style.backgroundColor = "powderblue"
+  x.style.transition = "all 3s ease-in-out";
+  x.style.transform = "rotate(-360deg)";
+  }
+
 function addRecommendation(){
     // Get the message of the new recommendation
     let recommendation = document.getElementById("new_recommendation");
-    // If the user has left a recommendation, display a pop-up
+   
     if (recommendation.value != null && recommendation.value.trim() != "") {
       console.log("New recommendation added");
-      //Call showPopup here
-     showPopup(true);
-      // Create a new 'recommendation' element and set it's value to the user's message
+      
+      showPopup(true);
       var element = document.createElement("div");
       element.setAttribute("class","recommendation");
       element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
